e78225725b059a2c7206e99dd787692249427be8
yunwisdom <qq695926920@163.com>
2020-01-16 17:18:24 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,41 @@
 const path = require('path');
+const webpack = require('webpack');
+//const createThemeColorReplacerPlugin = require('./config/plugin.config');
 
 function resolve(dir) {
     return path.join(__dirname, dir);
 }
 
+const isProd = process.env.NODE_ENV === 'production';
+
+const assetsCDN = {
+    // webpack build externals
+    externals: {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        vuex: 'Vuex',
+        axios: 'axios',
+    },
+    css: [],
+    js: [
+        '//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
+        '//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
+        '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
+        '//cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js',
+    ],
+};
+
 // vue.config.js
-module.exports = {
-    //可以将其设置为 false 以加速生产环境构建。
-    productionSourceMap: false,
-    //export NODE_ENV=production
+const vueConfig = {
+    configureWebpack: {
+        // webpack plugins
+        plugins: [
+            // Ignore all locale files of moment.js
+            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        ],
+        // if prod, add externals
+        externals: isProd ? assetsCDN.externals : {},
+    },
 
     pages: {
         index: {
@@ -16,12 +43,7 @@ module.exports = {
             chunks: ['chunk-vendors', 'chunk-common', 'index'],
         },
     },
-    configureWebpack: config => {
-        //生产环境取消 console.log
-        if (process.env.NODE_ENV === 'production') {
-            config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
-        }
-    },
+
     chainWebpack: config => {
         config.resolve.alias
             .set('@$', resolve('src'))
@@ -31,12 +53,42 @@ module.exports = {
             .set('@views', resolve('src/views'))
             .set('@layout', resolve('src/layout'))
             .set('@static', resolve('src/static'));
+
+        const svgRule = config.module.rule('svg');
+        svgRule.uses.clear();
+        svgRule
+            .oneOf('inline')
+            .resourceQuery(/inline/)
+            .use('vue-svg-icon-loader')
+            .loader('vue-svg-icon-loader')
+            .end()
+            .end()
+            .oneOf('external')
+            .use('file-loader')
+            .loader('file-loader')
+            .options({
+                name: 'assets/[name].[hash:8].[ext]',
+            });
+        // if prod is on
+        // assets require on cdn
+        if (isProd) {
+            config.plugin('html').tap(args => {
+                args[0].cdn = assetsCDN;
+                return args;
+            });
+        }
     },
 
     css: {
         loaderOptions: {
             less: {
-                modifyVars: {},
+                modifyVars: {
+                    // less vars，customize ant design theme
+                    // 'primary-color': '#F5222D',
+                    // 'link-color': '#F5222D',
+                    // 'border-radius-base': '4px'
+                },
+                // DO NOT REMOVE THIS LINE
                 javascriptEnabled: true,
             },
         },
@@ -53,5 +105,20 @@ module.exports = {
         },
     },
 
+    //可以将其设置为 false 以加速生产环境构建。
+    productionSourceMap: false,
+    //保存时提示
     lintOnSave: undefined,
+    // babel-loader no-ignore node_modules/*
+    transpileDependencies: [],
 };
+
+// preview.pro.loacg.com only do not use in your production;
+if (process.env.VUE_APP_PREVIEW === 'true') {
+    console.log('VUE_APP_PREVIEW', true);
+    // add `ThemeColorReplacer` plugin to webpack plugins
+    //vueConfig.configureWebpack.plugins.push(createThemeColorReplacerPlugin());
+}
+
+// vue.config.js
+module.exports = vueConfig;
