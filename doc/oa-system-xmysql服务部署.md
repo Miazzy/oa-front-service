016e238776b6c6fa0346efbe98c52f5c50b395ce
yunwisdom <qq695926920@163.com>
2020-01-17 15:25:41 +0800

    新增部署文档

---
+++ doc/oa-system-xmysql服务部署.md
@@ -0,0 +1,1 @@
+pm2 start xmysql -h yunwisdom.mysql.rds.aliyuncs.com -u yunwisdom -p yunwisdom@123 -d jeecg --watch -i 2
--- src/views/dashboard/Analysis.vue
+++ src/views/dashboard/Analysis.vue
@@ -847,4 +847,3 @@ thead.ant-table-thead {
 </style>
 
 
-pm2 start xmysql -h yunwisdom.mysql.rds.aliyuncs.com -u yunwisdom -p yunwisdom@123 -d jeecg --watch -i 2 
